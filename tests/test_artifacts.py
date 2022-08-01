ec8c7170265dd5ccb9b6622df6760c25048f8a9b
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2022-08-01 06:18:38 +0000

    pytest pass

--- tests/test_artifacts.py
+++ tests/test_artifacts.py
@@ -12,30 +12,6 @@ def sanity_check_for_dvc_file(model, dvc_file_path: str):
 
 
 @pytest.mark.asyncio
-async def test_mlcommons_resnet50():
-    sanity_check_for_dvc_file(
-        await nonblocking.ResNet50(),
-        "models/mlcommons_resnet50_v1.5_int8.onnx.dvc",
-    )
-
-
-@pytest.mark.asyncio
-async def test_mlcommons_ssd_mobilenet():
-    sanity_check_for_dvc_file(
-        await nonblocking.SSDMobileNet(),
-        "models/mlcommons_ssd_mobilenet_v1_int8.onnx.dvc",
-    )
-
-
-@pytest.mark.asyncio
-async def test_mlcommons_ssd_resnet34():
-    sanity_check_for_dvc_file(
-        await nonblocking.SSDResNet34(),
-        "models/mlcommons_ssd_resnet34_int8.onnx.dvc",
-    )
-
-
-@pytest.mark.asyncio
 async def test_yolov5_large():
     sanity_check_for_dvc_file(
         await nonblocking.YOLOv5l(),
