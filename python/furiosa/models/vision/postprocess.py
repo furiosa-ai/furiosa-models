54eb55ee6bcd0a51843037fbdd58e6d3a83bfe86
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2022-08-17 05:32:06 +0000

    rebase main

--- python/furiosa/models/vision/postprocess.py
+++ python/furiosa/models/vision/postprocess.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import List, Sequence
+
 import numpy as np
 
 
@@ -5,12 +8,50 @@ def sigmoid(x: np.ndarray) -> np.ndarray:  # pylint: disable=invalid-name
     return 1 / (1 + np.exp(-x))
 
 
-def calibration_box(bbox, width, height):
+def calibration_ltrbbox(bbox, width, height):
     bbox[:, 0] *= width
     bbox[:, 1] *= height
     bbox[:, 2] *= width
     bbox[:, 3] *= height
-
-    bbox[:, 2] -= bbox[:, 0]
-    bbox[:, 3] -= bbox[:, 1]
     return bbox
+
+
+@dataclass
+class LtrbBoundingBox:
+    left: float
+    top: float
+    right: float
+    bottom: float
+
+    def __iter__(self) -> List[float]:
+        return iter([self.left, self.top, self.right, self.bottom])
+
+
+@dataclass
+class ObjectDetectionResult:
+    boundingbox: LtrbBoundingBox
+    score: float
+    label: str
+    index: int
+
+
+def collate_data(
+    data: Sequence[Sequence[np.ndarray]], num_element: int, axis: int = 0
+) -> List[np.ndarray]:
+    """Collate lists of samples into batches.
+
+    Args:
+        data (List[List[np.ndarray]]): a list of a list of numpy array
+        num_element (int): data dimension
+        axis (int, optional): a mini-batch axis. Defaults to 0.
+
+    Returns:
+        List[np.ndarray]: a list of mini-batched numpy array.
+    """
+    batch_data = [[]] * num_element
+    for i in range(num_element):
+        feat = []
+        for b in range(len(data)):
+            feat.append(data[b][i])
+        batch_data[i] = np.concatenate(feat, axis=axis)
+    return batch_data
--- scripts/test_ssd_mobilenet.py
+++ scripts/test_ssd_mobilenet.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pytest
 
-from furiosa.models.vision import SSDMobileNet, ssd_mobilenet
+from furiosa.models.vision import SSDMobileNet
+from furiosa.models.vision.postprocess import collate_data
+from furiosa.models.vision.ssd_mobilenet import CLASSES, NUM_OUTPUTS, postprocess, preprocess
 from furiosa.registry import Model
 from furiosa.runtime import session
 
@@ -11,18 +13,29 @@ async def test_mlcommons_mobilessd_small_perf():
     m: Model = SSDMobileNet()
     test_image_path = "scripts/assets/cat.jpg"
 
-    assert len(ssd_mobilenet.CLASSES) == 92, f"Classes is 92, but {len(ssd_mobilenet.CLASSES)}"
-    with session.create(m.model) as sess:
-        true_bbox = np.array([[187.30786, 88.035324, 763.6886, 655.2937]], dtype=np.float32)
-        true_classid = np.array([17], dtype=np.int32)
-        true_confidence = np.array([0.97390455], dtype=np.float32)
-        preprocessed = ssd_mobilenet.preprocess(test_image_path)
-        outputs = sess.run(preprocessed[0]).numpy()
-        result = ssd_mobilenet.postprocess(outputs=outputs, extra_params=preprocessed[1])
-        assert len(result) == 3, "ssd_resnet34 output shape must be (1, 3)"
-        bbox, classid, confidence = result
-        assert np.array_equal(classid, true_classid), f"wrong classid: {classid}, expected 16(cat)"
-        assert np.sum(np.abs(bbox - true_bbox)) < 1e-3, f"bbox is different from expected value"
-        assert (
-            np.sum(np.abs(confidence - true_confidence)) < 1e-3
-        ), "confidence is different from expected value"
+    assert len(CLASSES) == 92, f"Classes is 92, but {len(CLASSES)}"
+    sess = session.create(m.model)
+    true_bbox = np.array([[187.30786, 88.035324, 950.99646, 743.3290239999999]], dtype=np.float32)
+    true_confidence = np.array([0.97390455], dtype=np.float32)
+    # For batch mode test, simply read two identical images.
+    batch_pre_image, batch_preproc_param = preprocess([test_image_path, test_image_path])
+    batch_feat = []
+    for pre_image in batch_pre_image:
+        feat = sess.run(np.expand_dims(pre_image, axis=0)).numpy()
+        assert len(feat) == NUM_OUTPUTS, f"model outputs expteds {NUM_OUTPUTS}"
+        batch_feat.append(feat)
+    batch_feat = collate_data(batch_feat, NUM_OUTPUTS, axis=0)
+    detected_result = postprocess(batch_feat, batch_preproc_param, confidence_threshold=0.3)
+    assert len(detected_result) == 2, "ssd_resnet34 output shape must be 2"
+    detected_result = detected_result[0]  # due to duplicated input image
+
+    assert len(detected_result) == 1, "detected object must be 1"
+    assert np.array_equal(
+        detected_result[0].label, 'cat'
+    ), f"wrong classid: {detected_result[0].label}, expected cat"
+    assert (
+        np.sum(np.abs(np.array(list(detected_result[0].boundingbox)) - true_bbox)) < 1e-3
+    ), f"bbox is different from expected value"
+    assert (
+        np.sum(np.abs(detected_result[0].score - true_confidence)) < 1e-3
+    ), "confidence is different from expected value"
