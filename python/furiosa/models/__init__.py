9a5edcd7dee8a17f718b38772e7c741c6e4c9d9f
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2022-08-19 11:01:42 +0000

    furioa

--- python/furiosa/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Furiosa Models"""
-from . import vision
-
-# NOTE: Import rust bindings here
-from .furiosa_models_native import *
-
-__all__ = ["vision"]
--- python/furiosa/models/vision/common/datasets/imagenet1k.py
+++
@@ -1,6 +0,0 @@
-import os
-
-ImageNet1k_CLASSES = [
-    label.strip()
-    for label in open(os.path.join(os.path.dirname(__file__), "imagenet-1k-label.txt")).readlines()
-]
