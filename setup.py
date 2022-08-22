ea8f953685436ce245868bd5b696cadf8581643d
MyeongGeun Shin <mg.shin@furiosa.ai>
2022-08-22 11:17:17 +0900

    Replace maturin with setuptools-rust

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_namespace_packages, setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    packages=find_namespace_packages(where="python"),
+    package_dir={"": "python"},
+    rust_extensions=[
+        RustExtension(
+            "furiosa.models.furiosa_models_native",
+            path="Cargo.toml",
+            binding=Binding.PyO3,
+            debug=False,
+        )
+    ],
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+)
