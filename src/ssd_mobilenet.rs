91d54a0cb43b827d57e9fda5f314cc4120e1589a
Hyunsik Choi <hyunsik@furiosa.ai>
2022-08-24 22:30:59 -0500

    Add test code and some part of postprocess

--- src/ssd_mobilenet.rs
+++ src/ssd_mobilenet.rs
@@ -1,15 +1,18 @@
+use itertools::Itertools;
 use mlperf_postprocess::common::graph::{create_graph_from_binary_with_header, GraphInfo};
-use mlperf_postprocess::common::ssd_postprocess::Postprocess;
+use mlperf_postprocess::common::{uninitialized_vec, ssd_postprocess::Postprocess};
 use mlperf_postprocess::ssd_small as native;
-use pyo3::{PyObject, PyRef, PyResult, Python};
-use pyo3::types::PyModule;
 use numpy::{
-    PyReadonlyArrayDyn,
     ndarray::ArrayViewD,
+    PyArrayDyn,
+    PyArray,
+    PyReadonlyArrayDyn,
 };
-
+use pyo3::{AsPyPointer, exceptions::PyValueError, PyErr, PyObject, PyRef, PyResult, Python, types::{PyList, PyModule}};
 
 use crate::{pyclass, pymethods, pymodule};
+
+const OUTPUT_NUM: usize = 12;
 
 #[pyclass]
 pub struct RustPostprocessor(native::RustPostprocessor);
@@ -33,9 +36,32 @@ impl RustPostprocessor {
         RustPostprocessor(native::RustPostprocessor::new(&graph))
     }
 
-    fn process(self_: PyRef<'_, Self>, inputs: PyReadonlyArrayDyn<'_, i8>) -> PyResult<()> {
-        eprintln!("inputs len: {}", inputs.len());
-        //self.0.postprocess(0, )
+    fn process(&self, inputs: &PyList) -> PyResult<()> {
+        // eprintln!("inputs len: [{}]", inputs.shape().iter().map(|s| s.to_string()).join(","));
+        // self.0.postprocess(0,);
+
+        if inputs.len() != OUTPUT_NUM {
+            return Err(PyErr::new::<PyValueError, _>(format!(
+                "expected 12 input tensors but got {}",
+                inputs.len()
+            )))
+        }
+
+        let mut memories: Vec<&[u8]> = unsafe { uninitialized_vec(OUTPUT_NUM) };
+        for tensor in inputs {
+            let tensor = tensor.downcast::<PyArrayDyn<i8>>()?;
+            if !tensor.is_c_contiguous() {
+                return Err(PyErr::new::<PyValueError, _>(
+                    "{}th tensor is not C-contiguous".to_string()))
+            }
+            let slice: &[u8] = unsafe {
+                let raw_slice = tensor.as_slice()?;
+                std::slice::from_raw_parts(raw_slice.as_ptr() as *const u8, raw_slice.len())
+            };
+            memories.push(slice);
+        }
+
+        let results = self.0.postprocess(0f32, &memories);
         Ok(())
     }
 }
@@ -46,6 +72,35 @@ impl CppPostprocessor {
     fn new(dfg: &[u8]) -> Self {
         let graph = create_graph_from_binary_with_header(dfg).unwrap();
         CppPostprocessor(native::CppPostprocessor::new(&graph))
+    }
+
+    fn process(&self, inputs: &PyList) -> PyResult<()> {
+        // eprintln!("inputs len: [{}]", inputs.shape().iter().map(|s| s.to_string()).join(","));
+        // self.0.postprocess(0,);
+
+        if inputs.len() != OUTPUT_NUM {
+            return Err(PyErr::new::<PyValueError, _>(format!(
+                "expected 12 input tensors but got {}",
+                inputs.len()
+            )))
+        }
+
+        let mut memories: Vec<&[u8]> = unsafe { uninitialized_vec(OUTPUT_NUM) };
+        for tensor in inputs {
+            let tensor = tensor.downcast::<PyArrayDyn<i8>>()?;
+            if !tensor.is_c_contiguous() {
+                return Err(PyErr::new::<PyValueError, _>(
+                    "{}th tensor is not C-contiguous".to_string()))
+            }
+            let slice: &[u8] = unsafe {
+                let raw_slice = tensor.as_slice()?;
+                std::slice::from_raw_parts(raw_slice.as_ptr() as *const u8, raw_slice.len())
+            };
+            memories.push(slice);
+        }
+
+        let results = self.0.postprocess(0f32, &memories);
+        Ok(())
     }
 }
 
--- tests/test_native_ssd_mobilenet.py
+++ tests/test_native_ssd_mobilenet.py
@@ -9,14 +9,11 @@ def test_rust_post_processor():
         dfg = file.read();
         processor = ssd_mobilenet.RustPostprocessor(dfg)
 
-        with session.create("models/mlcommons_ssd_mobilenet_v1_int8.onnx_truncated.onnx", device="npu0pe0-1") as sess:
+        with session.create("models/mlcommons_ssd_mobilenet_v1_int8.onnx_truncated.onnx") as sess:
             #print(f"input_num: {sess.input_num}")
 
-            outputs = sess.run(tensor.rand(sess.input(0)))
-            print("run is called")
+            for idx, input in enumerate(sess.outputs()):
+                print(f"{idx} - {input.shape}")
 
-            noutputs = outputs.numpy()
-            print("numpy is called")
-            #assert len(outputs) == 12
-
-            processor.process(np.array([[123], [123]], np.int8))
+            outputs = sess.run(tensor.rand(sess.input(0))).numpy()
+            processor.process(outputs)
