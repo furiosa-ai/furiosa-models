4e32331cab06698596e59e58e77e93a2e644fc3a
MyeongGeun Shin <mg.shin@furiosa.ai>
2022-09-14 14:33:04 +0900

    Reflect review, updates

---
+++ python/furiosa/models/client/api.py
@@ -0,0 +1,77 @@
+import time
+from typing import Callable, List, Optional
+
+from furiosa.models import vision
+from furiosa.models.vision import resnet50
+from furiosa.registry.model import Model
+from furiosa.runtime import session
+
+
+def normalize(text: str) -> str:
+    """Returns only casefolded alphabets and numbers
+
+    Args:
+        text: string to be normalized
+
+    Returns:
+        Normalized text
+    """
+    text = "".join(filter(lambda c: c.isalnum(), text))
+    return text.casefold()
+
+
+def get_model_list(filter_func: Optional[Callable[..., bool]] = None) -> List[List[str]]:
+    """Returns list of available model names and descriptions
+
+    Args:
+        filter_func: Filter function, called on each models and filter
+
+    Returns:
+        List of available model names and descriptions
+    """
+    filter_func = filter_func or (lambda _: True)
+    model_list = []
+    for model_name in vision.__all__:
+        model = getattr(vision, model_name)
+        if not filter_func(model):
+            continue
+        model_list.append([model_name, model.__doc__])
+    return model_list
+
+
+def list_processes(model_name: str):
+    # TODO: List available pre/postprocesses
+    ...
+
+
+def get_model(model_name: str) -> Optional[Model]:
+    """Get model for givne model name string
+
+    Args:
+        model_name: Model name
+
+    Returns:
+        A model which have name of the given argument
+    """
+    for name in vision.__all__:
+        if normalize(name) == normalize(model_name):
+            return getattr(vision, name)
+    return None
+
+
+def run_inference(model: Model, input_paths: List[str]):
+    net_inference_times = []
+    model_inner = model.load()
+    with session.create(model_inner) as sess:
+        for input_path in input_paths:
+            # FIXME: Get pre/post processes gracefully?
+            input = resnet50.preprocess(input_path)
+            start_time = time.perf_counter()
+            output = sess.run(input)
+            net_inference_times.append(time.perf_counter() - start_time)
+            output = output.numpy()  # To avoid calling __getitem__
+            output = resnet50.postprocess(output)
+            print(output)
+    average = sum(net_inference_times) / len(net_inference_times)
+    print(f"Average inference time: {average * 1000} ms")
+    print(f"Throughput: {1 / average}")
--- python/furiosa/models/client/main.py
+++ python/furiosa/models/client/main.py
@@ -1,6 +1,14 @@
 import argparse
+import logging
 from pathlib import Path
 import sys
+from typing import List
+
+from tabulate import tabulate
+
+from . import api
+
+logger = logging.getLogger(__name__)
 
 EXAMPLE: str = """example:
     # List available models
@@ -9,72 +17,88 @@ EXAMPLE: str = """example:
     # List Object Detection models
     furiosa-models list -t detect
 
-    # List available pre/post-processes for ResNet34
-    furiosa-models list -n ResNet34
+    # List available pre/post-processes for SSDResNet34
+    furiosa-models list --model SSDResNet34
 
-    # Run ResNet34 for images in `input` directory
-    furiosa-models classify -n ResNet34 -i input/
+    # Run SSDResNet34 for images in `./input` directory
+    furiosa-models run --model ssdresnet34 ./input/
 """
 
-parser = argparse.ArgumentParser(
-    prog="furiosa-models",
-    epilog=EXAMPLE,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-)
-subparsers = parser.add_subparsers(dest="command", required=True)
 
-# Note that common_parser is not used for `list` command
-common_parser = argparse.ArgumentParser(add_help=False)
-common_parser.add_argument(
-    "-i", "--input", type=str, help="REQUIRED: Input path (file or directory)", required=True
-)
-common_parser.add_argument("-n", "--name", type=str, help="REQUIRED: Model name", required=True)
-common_parser.add_argument("-pre", "--preprocess", type=str, help="Set preprocess type")
-common_parser.add_argument("-post", "--postprocess", type=str, help="Set postprocess type")
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="furiosa-models",
+        epilog=EXAMPLE,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        conflict_handler="resolve",
+    )
 
-list_parser = subparsers.add_parser(
-    "list", help="See available model/preprocesses/postprocesses list"
-)
-list_parser.add_argument(
-    "-t", "--type", type=str, help="Limits the task type (ex. classify, detect)"
-)
-list_parser.add_argument(
-    "-n",
-    "--name",
-    type=str,
-    help="Model name: if this argument is given, it will show the available pre/post processes",
-)
+    subparsers = parser.add_subparsers(
+        dest="command",
+        required=True,
+    )
 
-classification_parser = subparsers.add_parser(
-    "classify", parents=[common_parser], help="Run Image Classification"
-)
+    # Note that common_parser is not used for `list` command
+    common_parser = argparse.ArgumentParser(add_help=False)
 
-detection_parser = subparsers.add_parser(
-    "detect", parents=[common_parser], help="Run Object Detection"
-)
+    list_parser = subparsers.add_parser(
+        "list", help="See available models or pre/post-processes list"
+    )
+    list_parser.add_argument(
+        "-t", "--type", type=str, help="Limits the task type (ex. classify, detect)"
+    )
+    list_parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        help="Model name: if this argument is given, it will show the available pre/post processes",
+    )
+
+    inference_parser = subparsers.add_parser("run", parents=[common_parser], help="Run Inference")
+    inference_parser.add_argument("-pre", "--preprocess", type=str, help="Set preprocess type")
+    inference_parser.add_argument("-post", "--postprocess", type=str, help="Set postprocess type")
+    inference_parser.add_argument(
+        "-m", "--model", type=str, help="Model name (ignore case)", required=True
+    )
+    inference_parser.add_argument("input", type=str, help="Input path (file or directory)")
+
+    return parser.parse_args()
 
 
-def main():
-    args = parser.parse_args()
-    if args.command == "list":
-        print("list")
-        return
+def print_model_list(table: List[List[str]]):
+    header = ["Model name", "Model description"]
+    print(tabulate(table, headers=header, tablefmt="pretty"))
 
+
+def resolve_input_paths(input_path: Path) -> List[str]:
     # Process common arguments for running furiosa-models
-    # Create input file lists
-    input_path = Path(args.input)
+    # Create input file list
     if input_path.is_file():
-        input_paths = [input_path]
+        return [str(input_path)]
     elif input_path.is_dir():
         # Directory may containing image files
         image_extensions = {".jpg", ".jpeg", ".png"}
-        input_paths = (p for p in input_path.glob("**/*") if p.suffix.lower() in image_extensions)
+        return [str(p) for p in input_path.glob("**/*") if p.suffix.lower() in image_extensions]
     else:
         # Error
         print("error: invalid input path")
         return sys.exit(1)
 
-    if args.command == "classify":
-        print("classify")
-    elif args.command == "detect":
-        print("detect")
+
+def main():
+    args = parse_args()
+    command: str = args.command
+    model: str = args.model
+    input_path: str = args.input
+
+    if command == "list":
+        if model:
+            print(api.list_processes(model))
+        else:
+            print_model_list(api.get_model_list())
+    elif command == "run":
+        input_paths = resolve_input_paths(Path(input_path))
+        model = api.get_model(model)
+        if model is None:
+            logging.warn(f"Can't find model named {model}")
+        api.run_inference(model, input_paths)
