bc2faa5a03d1be29068446072e975c454c2242d8
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2022-09-16 00:47:30 +0000

    불필요 상속 제거

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import Extension, find_namespace_packages, setup
 from setuptools_rust import Binding, RustExtension
 
-
-class SharedObjectCTypesExtension(Extension):
-    pass
-
-
 setup(
     packages=find_namespace_packages(where="python"),
     package_dir={
         "": "python",
     },
     ext_modules=[
-        SharedObjectCTypesExtension(
+        Extension(
             "furiosa.models.vision.yolov5.box_decode.cbox_decode.box_decode.cbox_decode",
             [
                 "python/furiosa/models/vision/yolov5/box_decode/cbox_decode/box_decode/box_decode.cpp"
