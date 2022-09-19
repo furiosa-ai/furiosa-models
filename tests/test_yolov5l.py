10043a7bb188c7d7899f50e36a2bd330bdffb6c0
Heyokjune Jeon <hyeokjune.jeon@furiosa.ai>
2022-09-19 12:11:50 +0000

    Kevin 실험한: yolov5_l/m dvc로 다시 보정

--- tests/test_yolov5l.py
+++ tests/test_yolov5l.py
@@ -20,10 +20,7 @@ async def test_yolov5_large():
     assert len(CLASSES) == 10, "expected CLASS is 10"
 
     batch_im = [cv2.imread(test_image_path), cv2.imread(test_image_path)]
-    try:
-        sess = session.create(m.source, compile_config=m.compile_config(model_input_format='hwc'))
-    except Exception as e:
-        print("Exception", e)
+    sess = session.create(m.source, compile_config=m.compile_config(model_input_format='hwc'))
     batch_pre_img, batch_preproc_param = preprocess(batch_im, input_color_format="bgr")
     batch_feat = []
     for pre_image in batch_pre_img:
@@ -34,5 +31,5 @@ async def test_yolov5_large():
     assert len(detected_boxes) == 2, "batch axis is expected 2"
     # im_out = draw_bboxes(batch_im[0], list(map(astuple, detected_boxes[0])))
     # cv2.imwrite("yolov5l.jpg", im_out)
-    assert len(detected_boxes[0]) == 26, f"detected_boxes must be 26, got {len(detected_boxes[0])}"
+    assert len(detected_boxes[0]) == 27, f"detected_boxes must be 27, got {len(detected_boxes[0])}"
     sess.close()
