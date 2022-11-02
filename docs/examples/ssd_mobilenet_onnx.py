637bfc208780436dfc0cc87d679ecbfa9831252f
Hyunsik Choi <hyunsik@furiosa.ai>
2022-11-02 14:09:39 -0500

    Update the input images

--- docs/examples/ssd_mobilenet_onnx.py
+++ docs/examples/ssd_mobilenet_onnx.py
@@ -5,6 +5,6 @@ from furiosa.runtime import session
 model = SSDMobileNet.load()
 
 with session.create(model.source, batch_size=2) as sess:
-    image, context = preprocess(["tests/assets/cat.jpg"])
+    image, context = preprocess(["tests/assets/cat.jpg", "tests/assets/cat.jpg"])
     output = sess.run(image).numpy()
     postprocess(output, context=context)
