0876920e863ea257ef100d8506c99e68f8f0c34f
Hyunsik Choi <hyunsik@furiosa.ai>
2022-11-02 14:12:07 -0500

    Add latest and v0.8.0

---
+++ v0.8.0/examples/resnet50.py
@@ -0,0 +1,9 @@
+from furiosa.models.vision import ResNet50, resnet50
+from furiosa.runtime import session
+
+model = ResNet50.load()
+
+with session.create(model) as sess:
+    image = resnet50.preprocess("tests/assets/cat.jpg")
+    output = sess.run(image).numpy()
+    resnet50.postprocess(output)
