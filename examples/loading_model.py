ca4d857abd06aa5c1529a48120b3ac5117a38b4e
Hyunsik Choi <hyunsik@furiosa.ai>
2022-11-03 19:34:57 -0500

    Reorganize the document layout

--- examples/loading_model.py
+++
@@ -1,8 +0,0 @@
-from furiosa.models.vision import ResNet50
-from furiosa.registry import Model
-
-model: Model = ResNet50.load()
-
-print(model.name)
-print(model.format)
-print(model.metadata.description)
--- examples/resnet50.py
+++
@@ -1,9 +0,0 @@
-from furiosa.models.vision import ResNet50, resnet50
-from furiosa.runtime import session
-
-model = ResNet50.load()
-
-with session.create(model.enf) as sess:
-    image = resnet50.preprocess("tests/assets/cat.jpg")
-    output = sess.run(image).numpy()
-    resnet50.postprocess(output)
