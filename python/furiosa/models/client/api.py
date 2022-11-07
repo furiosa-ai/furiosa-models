a4cf2cf93972f1951f92c0db62e3b3874f07c19d
MyeongGeun Shin <mg.shin@furiosa.ai>
2022-11-07 17:14:17 +0900

    Introduce furiosa-models API, CLI

---
+++ python/furiosa/models/client/api.py
@@ -0,0 +1,80 @@
+from time import perf_counter
+from typing import Callable, List, Optional
+
+from tqdm import tqdm
+
+from furiosa.runtime import session
+
+from .. import vision
+from ..types import Model
+
+
+def normalize(text: str) -> str:
+    """Returns only casefolded alphabets and numbers
+
+    Args:
+        text: string to be normalized
+
+    Returns:
+        Normalized text
+    """
+    text = "".join(filter(lambda c: c.isalnum(), text))
+    return text.casefold()
+
+
+def get_model_list(filter_func: Optional[Callable[..., bool]] = None) -> List[List[str]]:
+    """Returns list of available model names and descriptions
+
+    Args:
+        filter_func: Filter function, called on each models and filter
+
+    Returns:
+        List of available model names and descriptions
+    """
+    filter_func = filter_func or (lambda _: True)
+    model_list = []
+    for model_name in vision.__all__:
+        model = getattr(vision, model_name)
+        if not filter_func(model):
+            continue
+        task_type = " ".join(
+            map(lambda x: x.capitalize(), model.__fields__["task_type"].default.name.split("_"))
+        )
+        # Model name, description, task type
+        model_list.append([model_name, model.__doc__, task_type])
+    return model_list
+
+
+def get_model(model_name: str) -> Optional[Model]:
+    """Get model for given model name string
+
+    Args:
+        model_name: Model name
+
+    Returns:
+        A model which have name of the given argument
+    """
+    for name in vision.__all__:
+        if normalize(name) == normalize(model_name):
+            return getattr(vision, name)
+    return None
+
+
+def run_inference(model: Model, input_paths: List[str]):
+    net_inference_times = []
+    model = model.load()
+    print(f"Running {len(input_paths)} inferences")
+    with session.create(model) as sess:
+        initial_time = perf_counter()
+        for input_path in tqdm(input_paths):
+            input, context = model.preprocess(input_path)
+            start_time = perf_counter()
+            model_output = sess.run(input)
+            net_inference_times.append(perf_counter() - start_time)
+            model_output = model_output.numpy()  # To avoid calling __getitem__
+            _final_output = model.postprocess(model_output, context)
+        total_time_elapsed = perf_counter() - initial_time
+    print(f"Total time elapsed: {total_time_elapsed} s")
+    average = sum(net_inference_times) / len(net_inference_times)
+    print(f"Average inference time: {average * 1000} ms")
+    print(f"Throughput: {1 / average}")
---
+++ python/furiosa/models/client/main.py
@@ -0,0 +1,112 @@
+import argparse
+import logging
+from pathlib import Path
+import sys
+from typing import List
+
+from tabulate import tabulate
+
+from . import api
+from ..types import ImageClassificationModel, ObjectDetectionModel
+
+logger = logging.getLogger(__name__)
+
+EXAMPLE: str = """example:
+    # List available models
+    furiosa-models list
+
+    # List Object Detection models
+    furiosa-models list -t detect
+
+    # List available pre/post-processes for SSDResNet34
+    furiosa-models list --model SSDResNet34
+
+    # Run SSDResNet34 for images in `./input` directory
+    furiosa-models run -m ssdresnet34 ./input/
+"""
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="furiosa-models",
+        epilog=EXAMPLE,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        conflict_handler="resolve",
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="command",
+        required=True,
+    )
+
+    list_parser = subparsers.add_parser(
+        "list", help="See available models or pre/post-processes list"
+    )
+    list_parser.add_argument(
+        "-t", "--type", type=str, help="Limits the task type (ex. classify, detect)"
+    )
+    list_parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        help="Model name: if this argument is given, it will show the available pre/post processes",
+    )
+
+    inference_parser = subparsers.add_parser("run", help="Run Inference")
+    inference_parser.add_argument("-pre", "--preprocess", type=str, help="Set preprocess type")
+    inference_parser.add_argument("-post", "--postprocess", type=str, help="Set postprocess type")
+    inference_parser.add_argument(
+        "-m", "--model", type=str, help="Model name (ignore case)", required=True
+    )
+    inference_parser.add_argument("input", type=str, help="Input path (file or directory)")
+
+    return parser.parse_args()
+
+
+def print_model_list(table: List[List[str]]):
+    header = ["Model name", "Model description", "Task type"]
+    print(tabulate(table, headers=header, tablefmt="pretty"))
+
+
+def resolve_input_paths(input_path: Path) -> List[str]:
+    """Create input file list"""
+    if input_path.is_file():
+        return [str(input_path)]
+    elif input_path.is_dir():
+        # Directory may containing image files
+        image_extensions = {".jpg", ".jpeg", ".png"}
+        return [
+            str(p.resolve())
+            for p in input_path.glob("**/*")
+            if p.suffix.lower() in image_extensions
+        ]
+    else:
+        # Error
+        print("error: invalid input path")
+        return sys.exit(1)
+
+
+def main():
+    args = parse_args()
+    command: str = args.command
+    model_name: str = args.model
+
+    if command == "list":
+        filter_type: str = args.type
+        if filter_type is None:
+            filter_func = lambda _: True
+        elif "detect" in filter_type.lower():
+            filter_func = lambda x: issubclass(x, ObjectDetectionModel)
+        elif "classif" in filter_type.lower():
+            filter_func = lambda x: issubclass(x, ImageClassificationModel)
+        else:
+            filter_func = lambda _: True
+            logger.warning(f"Unknwon type filter {filter_type}, showing all models...")
+        print_model_list(api.get_model_list(filter_func=filter_func))
+    elif command == "run":
+        input_path: str = args.input
+        input_paths = resolve_input_paths(Path(input_path))
+        model = api.get_model(model_name)
+        if model is None:
+            logging.warning(f"Can't find model named {model}")
+        api.run_inference(model, input_paths)
