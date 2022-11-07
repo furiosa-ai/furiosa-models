db3be55212206edca2dcd2d80de58a498b5b17fb
MyeongGeun Shin <mg.shin@furiosa.ai>
2022-11-07 17:15:07 +0900

    Do relative imports

--- python/furiosa/models/vision/__init__.py
+++ python/furiosa/models/vision/__init__.py
@@ -1,6 +1,6 @@
-from furiosa.models.vision.resnet50 import ResNet50
-from furiosa.models.vision.ssd_mobilenet import SSDMobileNet
-from furiosa.models.vision.ssd_resnet34 import SSDResNet34
-from furiosa.models.vision.yolov5 import YOLOv5l, YOLOv5m
+from ..vision.resnet50 import ResNet50
+from ..vision.ssd_mobilenet import SSDMobileNet
+from ..vision.ssd_resnet34 import SSDResNet34
+from ..vision.yolov5 import YOLOv5l, YOLOv5m
 
 __all__ = ["ResNet50", "SSDMobileNet", "SSDResNet34", "YOLOv5l", "YOLOv5m"]
