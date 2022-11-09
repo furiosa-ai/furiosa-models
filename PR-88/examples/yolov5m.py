bc0e61b40cc02ca3216c4db360d445a15bff2da4
Hyunsik Choi <hyunsik@furiosa.ai>
2022-11-08 20:51:26 -0600

    Reflect PR-88

--- PR-88/examples/yolov5m.py
+++ PR-88/examples/yolov5m.py
@@ -8,6 +8,6 @@ yolov5m = YOLOv5m.load()
 
 with session.create(yolov5m) as sess:
     image = cv2.imread("tests/assets/yolov5-test.jpg")
-    inputs, context = yolov5m.preprocess([image])
+    inputs, contexts = yolov5m.preprocess([image])
     output = sess.run(np.expand_dims(inputs[0], axis=0)).numpy()
-    yolov5m.postprocess(output, context=context)
+    yolov5m.postprocess(output, contexts=contexts)
