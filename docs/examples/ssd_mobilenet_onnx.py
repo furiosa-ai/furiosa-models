75785a103e66222c22aaf817ad44490db21dddd8
MyeongGeun Shin <mg.shin@furiosa.ai>
2022-12-05 10:49:14 +0900

    typos: context -> contexts

--- docs/examples/ssd_mobilenet_onnx.py
+++ docs/examples/ssd_mobilenet_onnx.py
@@ -5,6 +5,6 @@ images = ["tests/assets/cat.jpg", "tests/assets/cat.jpg"]
 
 mobilenet = SSDMobileNet.load()
 with session.create(mobilenet.source, batch_size=2) as sess:
-    inputs, context = mobilenet.preprocess(images)
+    inputs, contexts = mobilenet.preprocess(images)
     outputs = sess.run(inputs).numpy()
-    mobilenet.postprocess(outputs, context=context)
+    mobilenet.postprocess(outputs, contexts=contexts)
--- docs/examples/ssd_resnet34.py
+++ docs/examples/ssd_resnet34.py
@@ -4,6 +4,6 @@ from furiosa.runtime import session
 resnet34 = SSDResNet34.load()
 
 with session.create(resnet34) as sess:
-    image, context = resnet34.preprocess(["tests/assets/cat.jpg"])
+    image, contexts = resnet34.preprocess(["tests/assets/cat.jpg"])
     output = sess.run(image).numpy()
-    resnet34.postprocess(output, context=context)
+    resnet34.postprocess(output, contexts=contexts)
