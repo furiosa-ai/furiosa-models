f2ba5ad521e203df98eab19fb8faa91a15a38f71
Myeong-geun Shin <mg.shin@furiosa.ai>
2023-08-10 13:09:24 +0900

    fix: update ssd_resnet34 example and run in ci

--- docs/examples/ssd_resnet34.py
+++ docs/examples/ssd_resnet34.py
@@ -1,7 +1,7 @@
 from furiosa.models.vision import SSDResNet34
 from furiosa.runtime import session
 
-resnet34 = SSDResNet34()
+resnet34 = SSDResNet34("Python")
 
 with session.create(resnet34.model_source()) as sess:
     image, contexts = resnet34.preprocess(["tests/assets/cat.jpg"])
