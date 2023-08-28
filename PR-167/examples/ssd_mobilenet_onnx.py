6d5d6fb1f325da2d7d3a3a6659bf490e0f849b03
furiosa-infra <infra@furiosa.ai>
2023-08-28 04:33:00 +0000

    docs: auto generated by furiosa-models#167
    related commit: https://github.com/furiosa-ai/furiosa-models/commit/67c6de4

--- PR-167/examples/ssd_mobilenet_onnx.py
+++ PR-167/examples/ssd_mobilenet_onnx.py
@@ -8,11 +8,11 @@ image = ["tests/assets/cat.jpg"]
 
 mobilenet = SSDMobileNet()
 onnx_model: bytes = mobilenet.origin
-calib_range: dict = mobilenet.tensor_name_to_range
+tensor_name_to_range: dict = mobilenet.tensor_name_to_range
 
 # See https://furiosa-ai.github.io/docs/latest/en/api/python/furiosa.quantizer.html#furiosa.quantizer.quantize
 # for more details
-quantized_onnx = quantize(onnx_model, calib_range)
+quantized_onnx = quantize(onnx_model, tensor_name_to_range)
 
 with create_runner(quantized_onnx, compiler_config=compiler_config) as runner:
     inputs, contexts = mobilenet.preprocess(image, with_scaling=True)
