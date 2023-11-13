2b1323853315a55afb7360aec6a76b88af67764e
Myeong-geun Shin <mg.shin@furiosa.ai>
2023-11-13 16:32:04 +0900

    Simplify YOLOv7w6Pose example codes

--- docs/examples/yolov7_w6_pose_visualize.py
+++ docs/examples/yolov7_w6_pose_visualize.py
@@ -1,18 +1,14 @@
-from pathlib import Path
-
 import cv2
-import numpy as np
 
 from furiosa.models.vision import YOLOv7w6Pose
 from furiosa.runtime.sync import create_runner
 
-cwd = Path(__file__).parent
 yolo_pose = YOLOv7w6Pose()
 
 with create_runner(yolo_pose.model_source()) as runner:
     image = cv2.imread("tests/assets/pose_demo.jpg")
     inputs, contexts = yolo_pose.preprocess([image])
-    output = runner.run(np.expand_dims(inputs[0], axis=0))
+    output = runner.run(inputs)
     results = yolo_pose.postprocess(output, contexts=contexts)
     yolo_pose.visualize(image, results[0])
     cv2.imwrite("./pose_result.jpg", image)
