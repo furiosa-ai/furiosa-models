da0f74dba12453cad2b1e08fb7b0493eab082ed5
furiosa-infra <infra@furiosa.ai>
2023-11-16 04:18:54 +0000

    docs: auto generated by furiosa-models#173
    related commit: https://github.com/furiosa-ai/furiosa-models/commit/5ea6865

--- PR-173/examples/yolov7_w6_pose.py
+++ PR-173/examples/yolov7_w6_pose.py
@@ -1,16 +1,12 @@
-from pathlib import Path
-
 import cv2
-import numpy as np
 
 from furiosa.models.vision import YOLOv7w6Pose
 from furiosa.runtime.sync import create_runner
 
-cwd = Path(__file__).parent
 yolo_pose = YOLOv7w6Pose()
 
 with create_runner(yolo_pose.model_source()) as runner:
     image = cv2.imread("tests/assets/yolov5-test.jpg")
     inputs, contexts = yolo_pose.preprocess([image])
-    output = runner.run(np.expand_dims(inputs[0], axis=0))
+    output = runner.run(inputs)
     yolo_pose.postprocess(output, contexts=contexts)
